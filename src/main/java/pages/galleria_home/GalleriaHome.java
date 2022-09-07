c7e1e7c75b1291971e6400e38d34755ed454cbb2
Oleksii <jawuk.alex@gmail.com>
2022-09-07 18:11:45 +0300

    after 18.54 need to watch and implement

---
+++ src/main/java/pages/galleria_home/GalleriaHome.java
@@ -0,0 +1,12 @@
+package pages.galleria_home;
+
+import org.openqa.selenium.WebDriver;
+import pages.base.BasePage;
+
+public class GalleriaHome extends BasePage {
+
+    public GalleriaHome(WebDriver driver) {
+        super(driver);
+    }
+
+}
